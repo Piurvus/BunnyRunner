ef8b0b60e1a6bb609e172fe383a17de12a733a9b
Severin <Severin@PHILIPP>
2018-12-14 13:57:48 +0100

    background fixd

--- direct2d/Background.cpp
+++ direct2d/Background.cpp
@@ -2,13 +2,13 @@
 
 void Background::draw()
 {
-	sprites->Draw(xScreen1, -10.0f, 0.6, 1.0, true);
-	sprites->Draw(xScreen2, -10.0f, 0.6, 1.0, true);
-	sprites->Draw(xScreen3, -10.0f, 0.6, 1.0, true);
+	sprites->Draw((int)xScreen1, -10.0f, 0.6, 1.0, true);
+	sprites->Draw((int)xScreen2, -10.0f, 0.6, 1.0, true);
+	sprites->Draw((int)xScreen3, -10.0f, 0.6, 1.0, true);
 
 	grass->Draw(xgScreen1, 320.0f, 1.0);
 	grass->Draw(xgScreen2, 320.0f, 1.0);
-	grass->Draw(xgScreen3, 320.0f, 1.0);
+	//grass->Draw(xgScreen3, 320.0f, 1.0);
 }
 
 void Background::update(double speed)
@@ -27,19 +27,19 @@ void Background::update(double speed)
 	xScreen2 -= speed;
 	xScreen3 -= speed;	
 
-	if (xgScreen1 + 2040.0 <= 0) {
-		xgScreen1 = xgScreen1 + 3 * 2040.0;
+	if (xgScreen1 + 4080.0 <= 0) {
+		xgScreen1 = xgScreen1 + 2 * 4080.0;
 	}
-	else if (xgScreen2 + 2040.0 <= 0) {
-		xgScreen2 = xgScreen2 + 3 * 2040.0;
-	}
-	else if (xgScreen3 + 2040.0 <= 0) {
+	else if (xgScreen2 + 4080.0 <= 0) {
+		xgScreen2 = xgScreen2 + 2 * 4080.0;
+	}/*
+	else if (xgScreen3 + 4080.0 <= 0) {
 		xgScreen3 = xgScreen3 + 3 * 2040.0;
-	}
+	}*/
 
 	xgScreen1 -= 5.0 * speed;
 	xgScreen2 -= 5.0 * speed;
-	xgScreen3 -= 5.0 * speed;
+	//xgScreen3 -= 5.0 * speed;
 }
 
 Background::Background(Graphics * gfx):
@@ -47,12 +47,14 @@ Background::Background(Graphics * gfx):
 {
 	grass = new SpriteSheet(L"grass.png", gfx, 1.0f);
 	sprites = new SpriteSheet(L"backgroundnew.png", gfx, 1.0f);
-	xScreen1 = -0.1;
-	xScreen2 = 1223.9;
-	xScreen3 = 2.0 * xScreen2 + 0.1;
+	
+	xScreen1 = -3.0;
+	xScreen2 = 1221.0;
+	xScreen3 = 2.0 * xScreen2 + 3.0;
+	
 	xgScreen1 = -0.1;
-	xgScreen2 = 2039.9;
-	xgScreen3 = 2.0 * xgScreen2 + 0.1;
+	xgScreen2 = 4079.9;
+	//xgScreen3 = 2.0 * xgScreen2 + 0.1;
 }
 
 Background::~Background()
